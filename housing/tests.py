aec2d935d19d2a7be939409d03a0c5f1b83cc599
AvaneenP <avaneen@gmail.com>
2021-10-17 21:55:35 -0400

    added few dummy test cases - still need to add tests for Google Integration

--- housing/tests.py
+++ housing/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+class DummyTestCase(TestCase):
+    def setUp(self):
+        x = 1
+        y = 2
+    
+    def test_dummy_test_case(self):
+        self.assertEqual(1, 1)
+
+    def test_dummy_test_case_2(self):
+        self.assertEqual(2, 2)
