df05610db9be37125962e62b697e36f9104d8ef0
AvaneenP <avaneen@gmail.com>
2021-10-28 00:00:42 -0400

    added model for StudentHousing and added a view for displaying a list of the housing options

--- housing/urls.py
+++ housing/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 
 from . import views
 from allauth.account.views import LogoutView
@@ -6,4 +7,5 @@ from allauth.account.views import LogoutView
 urlpatterns = [
     path('', views.index, name='index'),
     path('logout', LogoutView.as_view()),
+    path('list', views.HousingListView.as_view(), name='studentHousingList')
 ]
--- housing/views.py
+++ housing/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.views import generic
+
+from .models import StudentHousing
+
+
 # Create your views here.
 def index(request):
     return render(request, 'housing/index.html')
+
+class HousingListView(generic.ListView):
+    template_name = 'housing/studentHousingList.html'
+    context_object_name = 'studentHousing_list'
+
+    def get_queryset(self):
+        """
+        Return all housing listings.
+        """
+        return StudentHousing.objects.all()
