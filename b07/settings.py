382ab9a5bcb421a8c09b8026e68e44f42b077b4e
haotianren12 <hr3xw@virginia.edu>
2021-11-07 15:45:29 -0500

    temporarily commented out the code for heroku postgreSQL

--- b07/settings.py
+++ b07/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=600)
+#DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
