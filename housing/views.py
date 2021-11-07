52888c443ef2edd0cec35f991dd7b024ba93c01b
AvaneenP <avaneen@gmail.com>
2021-11-07 13:54:21 -0500

    added a map to home page

--- housing/views.py
+++ housing/views.py
@@ -10,6 +10,14 @@ from .models import StudentHousing
 def index(request):
     return render(request, 'housing/index.html')
 
+def default_map(request):
+    # TODO: move this token to Django settings from an environment variable
+    # found in the Mapbox account settings and getting started instructions
+    # see https://www.mapbox.com/account/ under the "Access tokens" section
+    mapbox_access_token = 'pk.my_mapbox_access_token'
+    return render(request, 'housing/index.html', 
+                  { 'mapbox_access_token': mapbox_access_token })
+
 
 class HousingListView(generic.ListView):
     template_name = 'housing/studentHousingList.html'
