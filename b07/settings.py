5bd4ca7509e988a7c3863c4205607b7fb9187404
haotianren12 <hr3xw@virginia.edu>
2021-11-07 15:25:51 -0500

    another try

--- b07/settings.py
+++ b07/settings.py
@@ -164,7 +164,7 @@ SOCIALACCOUNT_PROVIDERS = {
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'project-b-07/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
