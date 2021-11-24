7ca41bc5a8a42997094a1d06d87032a25cfb6871
maggiemunday <mem5xwx@virginia.edu>
2021-11-24 10:56:30 -0500

    aws hosting images successfully for storage model, can only upload through admin

--- housing/forms.py
+++ housing/forms.py
@@ -1,9 +1,13 @@
 #created this document using https://docs.djangoproject.com/en/3.2/topics/forms/
 
 from django import forms
+from models import Storage
 
 class SuggestedListingName(forms.Form):
     name = forms.CharField(label='Name of housing option:', max_length=200)
 
 class SuggestedListingAddress(forms.Form):
     address = forms.CharField(label='Address:', max_length=500)
+
+class UploadImage(forms.Form):
+    image = forms.FileField()
