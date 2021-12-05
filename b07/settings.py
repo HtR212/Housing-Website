a97d38b4808ba2ef0ec505ecc3660fea3101b56e
Frank <hr3xw@virginia.edu>
2021-12-05 11:37:36 -0500

    disable postgres

--- b07/settings.py
+++ b07/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
     }
 }
 # For Postgres on heroku
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
