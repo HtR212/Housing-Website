20cd6fc5d78b5356829d89ce8e7a6a5f1eb30af7
Frank <hr3xw@virginia.edu>
2021-12-06 00:33:25 -0500

    debug

--- b07/settings.py
+++ b07/settings.py
@@ -103,8 +103,8 @@ DATABASES = {
     }
 }
 # For Postgres on heroku
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
