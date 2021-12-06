9840ff6afb568a3bf471fb1930b7940a3353e0dd
AvaneenP <avaneen@gmail.com>
2021-12-05 21:27:25 -0500

    finished adding all test cases

--- housing/admin.py
+++ housing/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import StudentHousing, Review, User, UserReview, SuggestedListings
+from .models import StudentHousing, Review, UserProfile, UserReview, SuggestedListings
 
 # Register your models here.
 admin.site.register(StudentHousing)
 admin.site.register(Review)
-admin.site.register(User)
+admin.site.register(UserProfile)
 admin.site.register(UserReview)
 admin.site.register(SuggestedListings)
 
