5b1d9068390a9fde967ab9c1a447a9922a0a7569
Frank <88000727+HaotianRen12@users.noreply.github.com>
2021-12-07 09:03:16 -0500

    Make Debug False

--- b07/settings.py
+++ b07/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7gc62g)lpn^+371r5cs*i95aes@$^k)67@0ftk6s!6-p(jd%k*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'off-grounds-housing-b07.herokuapp.com', 'project-b-07.herokuapp.com',
                  'test-project-b-07.herokuapp.com']
